ec38b13faabbb60923713e5f3a6a93cbdca02fe1
Ankit Kumar <ankitjames25@gmail.com>
2024-05-02 22:05:06 +0530

    version upgrade

--- Formula/zilla.rb
+++ Formula/zilla.rb
@@ -19,7 +19,7 @@ class Zilla < Formula
   homepage "https://github.com/aklivity/zilla"
   license "Aklivity Community License"
 
-  version "0.9.78"
+  version "0.9.79"
 
   url "https://raw.githubusercontent.com/aklivity/zilla/#{version}/cloud/docker-image/src/main/docker/zpm.json.template"
 
