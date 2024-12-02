1d420167830752a57361077d864f0ddb71774989
John Fallows <john.r.fallows@gmail.com>
2024-12-01 23:28:51 -0800

    Update zillabase version to 0.5.1

--- Formula/zillabase.rb
+++ Formula/zillabase.rb
@@ -19,7 +19,7 @@ class Zillabase < Formula
   homepage "https://github.com/aklivity/zillabase"
   license "Aklivity Community License"
   
-  version "0.5.0"
+  version "0.5.1"
 
   url "https://github.com/aklivity/zillabase"
 
