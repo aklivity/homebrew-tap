c560d6011b74602c579791626f96a8933119c8e2
John Fallows <john.r.fallows@gmail.com>
2024-12-16 19:20:42 -0800

    Update zilla version to 0.9.111

--- Formula/zilla.rb
+++ Formula/zilla.rb
@@ -19,7 +19,7 @@ class Zilla < Formula
   homepage "https://github.com/aklivity/zilla"
   license "Aklivity Community License"
 
-  version "0.9.105"
+  version "0.9.111"
 
   url "https://raw.githubusercontent.com/aklivity/zilla/#{version}/cloud/docker-image/src/main/docker/zpm.json.template"
 
